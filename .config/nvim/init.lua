7045726e350446128f5156d7046dfa8fa7924ec6
macrespo42 <macrespo@student.42.fr>
2025-02-16 21:58:03 +0100

    rebuild nvim config from scratch

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,19 @@
-require("user")
+require("config.lazy")
+
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>")
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>")
+vim.keymap.set("i", "jk", "<ESC>")
+
+vim.keymap.set("t", "<ESC>", "<C-\\><C-n>", { noremap = true, silent = true })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight when yanking (copying) text',
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
